61747f149b8f3fe5a44d607d47d582a30e8f2ab2
fauziramdaniii23 <fauziramdani234@gmail.com>
2023-11-25 11:51:56 +0700

    hero-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0ea5e9',
+        primary: '#5eead4',
         dark: '#0f172a',
       },
       animation: {
         'spin-slow': 'spin 8s linear infinite',
+        hallo: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
