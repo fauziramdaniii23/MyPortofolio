029bd65b39038568fa19c2a836662592cdee3252
fauziramdaniii23 <fauziramdani234@gmail.com>
2023-11-28 00:41:20 +0700

    add nav

---
+++ dist/js/script.js
@@ -0,0 +1,19 @@
+// NavButton
+const navButton = document.querySelector('#navButton');
+const navMenu = document.querySelector('#nav-menu');
+
+navButton.addEventListener('click', function () {
+  navButton.classList.toggle('nav-active');
+  navMenu.classList.toggle('hidden');
+});
+// Navbar
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const navFixed = header.offsetTop;
+
+  if (window.pageYOffset > navFixed) {
+    header.classList.add('navbar-fix');
+  } else {
+    header.classList.remove('navbar-fix');
+  }
+};
